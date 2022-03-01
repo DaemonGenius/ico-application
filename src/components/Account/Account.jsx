b7b84ca581ccb773df770a25836f8bb6232eca05
Christian <christian.jhardy.steinmann@gmail.com>
2022-03-01 23:13:37 +0200

    Updated ABI

--- src/components/Account/Account.jsx
+++ src/components/Account/Account.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 
 function Account(props) {
-
   return (
     <>
-      <div className="text-sm font-medium text-gray-900 truncate dark:text-white">
-        <h3>Address: {props.defaultAccount}</h3>
-      </div>
       <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-        <h3>Balance: {props.userBalance}</h3>
+        <div className="p-4 max-w-md bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+          <div className="flex-1 min-w-0">
+            <h3>Address: {props.defaultAccount}</h3>
+            <h3>Balance:</h3>
+          </div>
+          <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+            {props.userBalance} ETH
+          </div>
+          <div className="items-center text-base font-semibold text-gray-900 dark:text-white">
+            {props.userBalance} ETH
+          </div>
+        </div>
       </div>
     </>
   );
--- src/hooks/account.jsx
+++ src/hooks/account.jsx
@@ -1,16 +1,38 @@
 import { useWeb3React } from "@web3-react/core";
 import { useEffect, useState } from "react";
+import Steinnegen from "@DaemonGenius/ico/ico-steinnegen/build/contracts/Steinnegen.json";
+import Web3 from "web3";
+import ganache from "ganache"
 
 export default function useAccount() {
   const { account, library } = useWeb3React();
   const [balance, setBalance] = useState();
+  
+  const web3 = new Web3(ganache.provider());
 
   useEffect(async () => {
     if (account) {
-       let val = await library.eth.getBalance(account);
-       setBalance(library.utils.fromWei(val, 'ether'));
+      try {
+        // get contract instance
+        const networkId = await web3.eth.net.getId();
+        const deployedNetwork = Steinnegen.networks[networkId];
+
+        let instance = new web3.eth.Contract(
+          Steinnegen.abi,
+          account,
+        );
+        console.log(await instance);
+  
+      } catch (error) {
+        console.error(error);
+      }
+   
+
+      let val = await library.eth.getBalance(account);
+      console.log(await library);
+      setBalance(library.utils.fromWei(val, "ether"));
     }
-  }, [account, library])
+  }, [account, library]);
 
   return balance;
 }
