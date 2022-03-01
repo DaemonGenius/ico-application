82b243e2cb1d571f0b4045f83de901e4fe5d5a9c
Christian <christian.jhardy.steinmann@gmail.com>
2022-03-02 00:27:42 +0200

    Updated lock and account.jsx

--- src/hooks/account.jsx
+++ src/hooks/account.jsx
@@ -13,19 +13,27 @@ export default function useAccount() {
   useEffect(async () => {
     if (account) {
       try {
-        // get contract instance
-        const networkId = await web3.eth.net.getId();
-        const deployedNetwork = Steinnegen.networks[networkId];
-
-        let instance = new web3.eth.Contract(
+     
+        let instance = new library.eth.Contract(
           Steinnegen.abi,
           "0xd1cc5b72A8571c24c5293BA3B1b3C06435bD5Fdf",
            {
-            from: '0xd1cc5b72A8571c24c5293BA3B1b3C06435bD5Fdf', // default from address
+            from: "0x06fE574C77EA8E2dD17B69bc09853B49EE97Ad8b", // default from address
             gasPrice: '20000000000' // default gas price in wei, 20 gwei in this case
         });
         console.log(await instance.methods.name().call());
-  
+        console.log(await instance.methods.symbol().call());
+        console.log(await instance.methods.totalSupply().call());
+        console.log(library.utils.fromWei(await instance.methods.balanceOf("0x482f80AC8C15c450fC866F17d9faC1003eB21a06").call(), "ether"));
+        // await instance.methods.approve("0x06fE574C77EA8E2dD17B69bc09853B49EE97Ad8b", 200)
+        // await instance.methods.approve("0x482f80AC8C15c450fC866F17d9faC1003eB21a06", 200)
+        // await instance.methods.allowance("0x06fE574C77EA8E2dD17B69bc09853B49EE97Ad8b", "0x482f80AC8C15c450fC866F17d9faC1003eB21a06");
+     
+
+      // let receipt = await instance.methods.transfer("0x482f80AC8C15c450fC866F17d9faC1003eB21a06", 2000).send();
+
+      // console.log(receipt)
+        
       } catch (error) {
         console.error(error);
       }
